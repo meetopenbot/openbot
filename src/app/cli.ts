f966f307d396c13cfbd39ede3c029cfe3ac71f80
David Daraselia <d.daraselia@gmail.com>
2026-05-02 02:10:48 +0400

    0.2.14

--- src/app/cli.ts
+++ src/app/cli.ts
@@ -25,7 +25,7 @@ function checkNodeVersion() {
 
 checkNodeVersion();
 
-program.name('openbot').description('OpenBot CLI').version('0.2.13');
+program.name('openbot').description('OpenBot CLI').version('0.2.14');
 
 program
   .command('start')
