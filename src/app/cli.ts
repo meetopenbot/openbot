4e6d95a8c13e9de0a8d9e756bcb368cc9e77fa47
ddaras <d.daraselia@gmail.com>
2026-05-17 03:04:46 +0400

    0.3.6

--- src/app/cli.ts
+++ src/app/cli.ts
@@ -25,7 +25,7 @@ function checkNodeVersion() {
 
 checkNodeVersion();
 
-program.name('openbot').description('OpenBot CLI').version('0.3.4');
+program.name('openbot').description('OpenBot CLI').version('0.3.6');
 
 program
   .command('start')
